bba7a75f42c39c9a935372606b58dec84bcb0964
Vigo Yang <vigo.yang@farfetch.com>
2024-07-26 14:45:16 +0800

    refine search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
 
 export function SearchBar(props: any) {
   const { onSubmit } = props;
+  const [value, setValue] = useState("");
 
   function submit(e: any) {
     if (e.key === "Enter" && e.target.value.trim() !== "") {
       onSubmit(e.target.value.trim());
+      setValue("");
     }
+  }
+  function onTextChange(e: any) {
+    setValue(e.target.value);
   }
   return (
     <div>
@@ -16,6 +22,8 @@ export function SearchBar(props: any) {
         onKeyUp={submit}
         className={s.input}
         type="text"
+        value={value}
+        onChange={onTextChange}
         placeholder={"Search a TV show you may like"}
       />
     </div>
