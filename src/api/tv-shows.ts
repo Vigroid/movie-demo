240f7e40b6b638d72b21386fbfef01b0dc280ed0
Vigo Yang <vigo.yang@farfetch.com>
2024-07-26 17:26:27 +0800

    HIde api key

--- src/api/tv-shows.ts
+++ src/api/tv-shows.ts
@@ -1,26 +1,30 @@
 // import axios from "axios";
-// import axios from "axios";
 import axios from "axios";
-import { FAKE_POP_TV, FAKE_RECOMMENDATIONS } from "./fake_data";
-import { API_KEY_PARAMS, BASE_URL } from "../config";
+// import { FAKE_POP_TV, FAKE_RECOMMENDATIONS } from "./fake_data";
+import { BASE_URL } from "../config";
 // import { API_KEY_PARAMS, BASE_URL } from "../config";
 
 export class TVShowAPI {
+
+    static fetchAPIKey() {
+        return process.env.REACT_APP_API_KEY_PARAMS
+    }
+
     static async fetchPopulars() {
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAMS}`)
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${TVShowAPI.fetchAPIKey()}`)
         return response.data.results
         // return FAKE_POP_TV
     }
 
     static async fetchRecommendations(tvShowId: string) {
-        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAMS}`)
+        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations?api_key=${TVShowAPI.fetchAPIKey()}`)
         return response.data.results
         // return FAKE_RECOMMENDATIONS
     }
 
     static async fetchByTitle(title: string) {
         const response = await axios.get(
-            `${BASE_URL}search/tv${API_KEY_PARAMS}&query=${title}`
+            `${BASE_URL}search/tv?api_key=${TVShowAPI.fetchAPIKey()}&query=${title}`
         )
         return response.data.results
     }
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/"
-const API_KEY_PARAMS = "?api_key=06597dec473d87a69ea18c4366f85019"
+const API_KEY_PARAMS = "06597dec473d87a69ea18c4366f85019"
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
 const SMALL_ITEM_BASE_URL = "https://image.tmdb.org/t/p/w300"
 
